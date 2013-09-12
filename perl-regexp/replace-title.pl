0baa79b880e1e7b53cb700c9ba0b9362c310e20d
Vit Karpov <viktor.s.karpov@gmail.com>
2013-09-12 15:03:36 +0400

    finished task, adds test-regexp file

--- perl-regexp/replace-title.pl
+++ perl-regexp/replace-title.pl
@@ -1,20 +1,28 @@
 # читаем каждую строку из файлов, которые пришли на стандартный ввод
 while (<@ARGV>) {
   $file = $_;
+  # получаем дескрипторы файлов для каждого, который пришел на ввод
   open(SOURCE, "< $file") or die "Couldn't open $file: $!\n";
-  open(COPY, "> $file.copy") or die "Couldn't open $file: $!\n";
+  open(COPY, "> $file.tmp") or die "Couldn't open $file: $!\n";
 
-  # select(COPY);
+  # будем писать во временные файлы
+  select(COPY);
+  # перебираем каждую строку исходного файла
   while (<SOURCE>) {
-    if ($_ =~ m/<title>.*(ШРИ)<\/title>/mi) {
+    # если нашли ШРИ между тегами title
+    if ($_ =~ m/<title>.*(ШРИ.*)<\/title>/mi) {
+      # заменим на ШРИ на КИТ
       $_ =~ s/ШРИ/КИТ/;
-    }  
-    print 1, $_ or die "couldn'r write $file: $!\n";
+    }
+    print $_ or die "couldn'r write $file: $!\n";
   }  
 
+  # удаляем дескприторы
   close(SOURCE) or die "Couldn't close $file: $!\n";
   close(COPY)  or die "Couldn't close $file: $!\n";
+  # заменяем оригинальные файлы, а последние кладем в бэкап
+  # просто добавляем origin к имени
   rename($file, "$file.origin");
-  rename("$file.copy", $file);
+  rename("$file.tmp", $file);
 }
 END { close(STDOUT) or die "couldn't close STDOUT: $!" }
--- perl-regexp/test-regexp.pl
+++ perl-regexp/test-regexp.pl
@@ -1,6 +1,6 @@
-$string = "     <title>Привет, я в ШРИ</title>";
+$string = "  <title>Командная строка — наше все. ШРИ 2013. Виктор Ашик.</title>";
 print $string, "\n";
-if ($string =~ m/<title>.*(ШРИ)<\/title>/mi) {
+if ($string =~ m/<title>.*(ШРИ.*)<\/title>/mi) {
   $string =~ s/ШРИ/КИТ/;
 }
 print $string, "\n";
