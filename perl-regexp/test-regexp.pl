023c834ba418480347c617062227af3f4a20e6eb
Vit Karpov <viktor.s.karpov@gmail.com>
2013-09-12 23:40:07 +0400

    refactoring: удалить тестовый файл для проверки регулярки, убрать match и оставить только replace

--- perl-regexp/test-regexp.pl
+++
@@ -1,6 +0,0 @@
-$string = "  <title>Командная строка — наше все. ШРИ 2013. Виктор Ашик.</title>";
-print $string, "\n";
-if ($string =~ m/<title>.*(ШРИ.*)<\/title>/mi) {
-  $string =~ s/ШРИ/КИТ/;
-}
-print $string, "\n";
